c89e3b98921ed33c8c561f4f9facf0eb4c2ada10
Prashish Bhandari <89494812+prashishb@users.noreply.github.com>
2021-12-01 22:16:27 -0500

    PUT update User

--- api/server.js
+++ api/server.js
@@ -44,4 +44,20 @@ server.post('/api/register', async (req, res) => {
   }
 });
 
+// PUT /api/users/:id
+server.put('/api/users/:id', async (req, res) => {
+  const { id } = req.params;
+  const { username, password } = req.body;
+  try {
+    const updatedUser = await User.update(id, { username, password });
+    if (!updatedUser) {
+      res.status(404).json({ message: `User by id ${id} not found` });
+    } else {
+      res.status(200).json(updatedUser);
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 module.exports = server;
