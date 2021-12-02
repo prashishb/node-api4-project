c66fdca003dcde31cee91b3d5747d32ffd847524
Prashish Bhandari <89494812+prashishb@users.noreply.github.com>
2021-12-01 22:19:26 -0500

    DELETE user by ID

--- api/server.js
+++ api/server.js
@@ -60,4 +60,19 @@ server.put('/api/users/:id', async (req, res) => {
   }
 });
 
+// DELETE /api/users/:id
+server.delete('/api/users/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedUser = await User.remove(id);
+    if (!deletedUser) {
+      res.status(404).json({ message: `User by id ${id} not found` });
+    } else {
+      res.status(200).json(deletedUser);
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 module.exports = server;
