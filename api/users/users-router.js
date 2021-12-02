6f6a3f18adefbd3fd0bc4430962b76567d96f2bb
Prashish Bhandari <89494812+prashishb@users.noreply.github.com>
2021-12-01 21:53:17 -0500

    GET user by ID

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -13,4 +13,14 @@ router.get('/', async (req, res) => {
   }
 });
 
+// GET /api/users/:id
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 module.exports = router;
